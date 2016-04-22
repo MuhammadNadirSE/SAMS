af88a072103f033791f4db8fa252fc5a83b7ba73
talhahanjra <talhahanjra@gmail.com>
2016-04-22 13:38:45 +0500

    new models

---
+++ TMD.Models/DomainModels/Seed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMD.Models.DomainModels
+{
+    public class Seed
+    {
+        public int SeedID { get; set; }
+        public string SeedCode { get; set; }
+        public string ValueCode { get; set; }
+        public Nullable<int> Value { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<SeedValue> SeedValues { get; set; }
+    }
+}
---
+++ TMD.Models/DomainModels/SeedValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMD.Models.DomainModels
+{
+    public class SeedValue
+    {
+        public int SeedValueID { get; set; }
+        public Nullable<int> SeedID { get; set; }
+        public string ValueCode { get; set; }
+        public Nullable<int> ValueId { get; set; }
+        public string Description { get; set; }
+
+        public virtual Seed Seed { get; set; }
+    }
+}
