d355a6accc96968295d42b147945a2b9c94bda56
talhahanjra <talhahanjra@gmail.com>
2016-05-09 00:11:58 +0500

    Notifications Controller Etc

---
+++ TMD.Web/Controllers/NotificationController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using TMD.Interfaces.IServices;
+using TMD.Models.DomainModels;
+
+namespace TMD.Web.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly INotificationService notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            this.notificationService = notificationService;
+        }
+        //
+        // GET: /Notification/
+        public ActionResult Index()
+        {
+            List<Notification> Notifications =
+                notificationService.GetAllNotificationsByUserId(User.Identity.GetUserId()).ToList();
+            return View(Notifications);
+        }
+
+        //
+        // GET: /Notification/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Notification/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Notification/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Notification/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Notification/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Notification/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Notification/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
